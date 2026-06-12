7d767244c521cccb01e84ddb5a43523cbf311d66
Egidius <git@sermak.xyz>
2026-06-12 10:37:44 +0200

    2026-06-12 10:37:44

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -5,43 +5,43 @@
 }:
 
 {
-  home-manager.users.user {
-  programs.firefox = {
-    enable = true;
-    nativeMessagingHosts = [ pkgs.keepassxc ];
-    languagePacks = [
-      "en"
-      "de"
-      "tg"
-      "nl"
-      "la"
-    ];
+  home-manager.users.user = {
+    programs.firefox = {
+      enable = true;
+      nativeMessagingHosts = [ pkgs.keepassxc ];
+      languagePacks = [
+        "en"
+        "de"
+        "tg"
+        "nl"
+        "la"
+      ];
 
-    policies = {
-      HardwareAcceleration = true;
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      DontCheckDefaultBrowser = true;
-      DefaultDownloadDirectory = "/home/user/downloads";
-    };
-
-    profiles.default = {
-      id = 0;
-      isDefault = true;
-
-      search = {
-        default = "ddg";
-        force = true; # prevents Firefox from overriding it on updates
+      policies = {
+        HardwareAcceleration = true;
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        DontCheckDefaultBrowser = true;
+        DefaultDownloadDirectory = "/home/user/downloads";
       };
 
-      settings = {
-        # enable compact mode (hidden option since Firefox 89)
-        "browser.compactmode.show" = true;
-        "browser.uidensity" = 1;
-        "full-screen-api.ignore-widgets" = true;
-      };
+      profiles.default = {
+        id = 0;
+        isDefault = true;
 
+        search = {
+          default = "ddg";
+          force = true; # prevents Firefox from overriding it on updates
+        };
+
+        settings = {
+          # enable compact mode (hidden option since Firefox 89)
+          "browser.compactmode.show" = true;
+          "browser.uidensity" = 1;
+          "full-screen-api.ignore-widgets" = true;
+        };
+
+      };
     };
   };
-};
 }
