7290ad02a05211471a93c04e20e462f240a89408
Egidius <git@sermak.xyz>
2026-06-15 11:33:53 +0200

    2026-06-15 11:33:53

--- modules/theme.nix
+++ modules/theme.nix
@@ -1,48 +1,36 @@
 { config, lib, pkgs, ... }:
 
 {
-  home-manager.users.user = {
-      home.packages = with pkgs; [
-        kdePackages.breeze-gtk
+home-manager.users.user = {
+       gtk = {
+        enable = true;
+        theme = {
+          package = pkgs.kdePackages.breeze-gtk;
+          name = "Breeze-Dark";
+        };
 
-        # Fonts
-        nerd-fonts.jetbrains-mono
-        nerd-fonts.symbols-only
-	    nerd-fonts.sauce-code-pro
-        symbola
-      ];
+        font = {
+          name = "SauceCodePro Semibold";
+          size = 10;
+        };
 
-    gtk = {
-      enable = true;
-      theme = {
-        package = pkgs.kdePackages.breeze-gtk;
-        name = "Breeze-Dark";
+        cursorTheme = {
+          name = "breeze_cursors";
+          size = 24;
+        };
+
+        iconTheme = {
+          package = pkgs.flat-remix-icon-theme;
+          name = "Flat-Remix-Green-Dark";
+        };
+
+        gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+        };
+
+        gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+        };
       };
-
-      font = {
-        name = "SauceCodePro Semibold";
-        size = 10;
-      };
-
-      cursorTheme = {
-        name = "breeze_cursors";
-        size = 24;
-      };
-
-      iconTheme = {
-        package = pkgs.flat-remix-icon-theme;
-        name = "Flat-Remix-Green-Dark";
-      };
-
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-      };
-
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-      };
-    };
-  }
-
-
+};
 }
